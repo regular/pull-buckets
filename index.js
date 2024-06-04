4b15dfbb4db0dfa4e94a3571662494bc150fa385
Jan Bölsche <regular.gonzales@googlemail.com>
2024-06-04 10:11:28 +0200

    Initial

---
+++ index.js
@@ -0,0 +1,83 @@
+//jshint -W033
+//jshint -W018
+//jshint  esversion: 11
+const pull = require('pull-stream')
+const copy = require('deep-copy')
+
+module.exports = function(fitsBucket, add, opts) {
+  opts = opts || {}
+  let {timeout, initial,max_age} = opts
+  if (max_age == undefined) max_age = 512
+
+  let end, timer, bucket = initial, reading
+  let rec_cnt = 0
+  const cbs = []
+  const buff = []
+
+  return function (read) {
+
+    return function (abort, cb) {
+      cbs.push(cb)
+
+      if (buff.length) {
+        cb = cbs.shift()
+        const [err, acc] = buff.shift()
+        if (err) return cb(err)
+        return cb(null, acc)
+      }
+      if (end) return flush(end)
+      if (reading) return
+
+      function flush(err, acc) {
+        buff.push([err, acc])
+        cb = cbs.shift()
+        if (cb) {
+          if (timer) clearTimeout(timer)
+          timer = null
+          const [err, acc] = buff.shift()
+          if (err) return cb(err)
+          return cb(null, acc)
+        }
+      }
+
+      function setTimer() {
+        if (timer) clearTimeout(timer)
+        if (!timeout) return
+        timer = setTimeout(()=>{
+          timer = null
+          if (bucket !== null && bucket !== undefined) {
+            flush(null, copy(bucket))
+          }
+        }, timeout)
+      }
+
+      setTimer()
+
+      function slurp() {
+        reading = true
+        read(abort, (err, data) =>{
+          reading = false
+          if (err) {
+            end = err
+            if (bucket) flush(null, bucket)
+            else flush(err)
+            return
+          }
+
+          if (!bucket || fitsBucket(bucket, data)) {
+            bucket = add(bucket, data)
+            if (max_age && ++rec_cnt >= max_age) {
+              rec_cnt = 0
+              flush(null, copy(bucket))
+            }
+            return slurp()
+          } else {
+            flush(null, bucket)
+            bucket = add(undefined, data)
+          }
+        })
+      }
+      slurp()
+    }
+  }
+}
---
+++ test.js
@@ -0,0 +1,129 @@
+//jshint esversion:11
+//jshint -W033
+const test = require('tape')
+const pull = require('pull-stream')
+const Stream = require('.')
+
+test('aggregates values', t=>{
+  pull(
+    pull.values([1,2,10,11,20,25]),
+    Stream(fitsBucket, add),
+    pull.collect( (err, data)=>{
+      t.deepEqual(data, [
+        {tens: 0, sum: 3, l: [ 1, 2 ]},
+        {tens: 1, sum: 21, l: [ 10, 11 ]},
+        {tens: 2, sum: 45, l: [ 20, 25 ]} 
+      ])
+      t.end()
+    })
+  )
+})
+
+test('initial', t=>{
+  pull(
+    pull.values([10]),
+    Stream(fitsBucket, add, {
+      initial: {
+        tens: 1, l:[], sum: 100
+      }
+    }),
+    pull.collect( (err, data)=>{
+      t.deepEqual(data, [
+        { tens: 1, sum: 110, l: [ 10 ] }
+      ])
+      t.end()
+    })
+  )
+})
+
+test('initial, then end', t=>{
+  pull(
+    pull.values([]),
+    Stream(fitsBucket, add, {
+      initial: {
+        tens: 1, l:[], sum: 100
+      }
+    }),
+    pull.collect( (err, data)=>{
+      t.deepEqual(data, [
+        { tens: 1, sum: 100, l: [] }
+      ])
+      t.end()
+    })
+  )
+})
+
+test('dont stall', t=>{
+  pull(
+    timedSource([
+      [0, 1],
+      [0, 2],
+      [200, 3],
+      [0, 10],
+      [0, 11],
+      [0, 20],
+      [200, 25],
+    ]),
+    Stream(fitsBucket, add, {timeout: 100}),
+    pull.collect( (err, data)=>{
+      t.deepEqual(data, [
+        {tens: 0, sum: 3, l: [ 1, 2 ] },
+        {tens: 0, sum: 6, l: [ 1, 2, 3 ] },
+        {tens: 1, sum: 21, l: [ 10, 11 ] },
+        {tens: 2, sum: 20, l: [ 20 ] },
+        {tens: 2, sum: 45, l: [ 20, 25 ] }
+      ])
+      t.end()
+    })
+  )
+})
+
+test('max_age', t=>{
+  pull(
+    pull.values([1,2,4, 10,11,12]),
+    Stream(fitsBucket, add, {max_age: 2}),
+    pull.collect( (err, data)=>{
+      t.deepEqual(data, [
+        {tens: 0, sum: 3, l: [ 1, 2 ] },
+        {tens: 0, sum: 7, l: [ 1, 2, 4 ] },
+        {tens: 1, sum: 21, l: [ 10, 11 ] },
+        {tens: 1, sum: 33, l: [ 10, 11, 12 ] }
+      ])
+      t.end()
+    })
+  )
+})
+
+
+// - - - -
+
+function bucketKey(n) {
+  return (n / 10) << 0
+}
+
+function add(b, n) {
+  b = b || {
+    tens: bucketKey(n),
+    l: [],
+    sum: 0
+  }
+  b.sum += n
+  b.l = b.l.concat([n])
+  return b
+}
+
+function fitsBucket({tens}, n) {
+  return bucketKey(n) == tens
+}
+
+function timedSource(data) {
+  return pull(
+    pull.values(data),
+    pull.asyncMap(function(item, cb) {
+      setTimeout(function() {
+        cb(null, item[1])
+      }, item[0]);
+    })
+  )
+}
+
